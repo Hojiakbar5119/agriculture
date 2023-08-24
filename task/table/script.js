3b4bc9e20f47a3efeb7330ac575f05089eafecb1
Hojiakbar5119 <Abdulhakimovhojiakbar123@gmail.com>
2023-08-24 20:04:31 +0500

    men ko'p narsalarni organgapman

---
+++ task/table/script.js
@@ -0,0 +1,46 @@
+// let str1="a";
+// let str2="A";
+
+// console.log(str1===str2);
+// console.log(str1==str2);  //? ASCI codedan ishlatilarkan
+
+// console.log(" \t \n" +2 );
+// console.log(" \t \n" +2 );
+// console.log(" \t \n" +2 );
+
+// console.log(null+1); //1
+// console.log(undefined+1); //NAN
+// console.log(undefined-1); //NAN
+// console.log(undefined*3); //NAN
+// console.log(undefined/1); //NAN
+
+// console.log("2" > "12");
+// console.log(undefined == null);
+// console.log(undefined === null); //? type larni ham tekshiradi shuning uchun false olgan ekan
+
+// if ("") {
+//     console.log("hello");
+// }
+
+// console.log(false==+"\n0\n");
+
+// console.log(1 !== "0" || (false && "hey")); //! true
+// console.log(1 !== "0" || (true && "hey")); //! hey
+// console.log(true && "hey");
+
+// console.log(1 !== "0");
+// console.log(true||(true && "hey"));
+// debugger
+// console.log((true && "hey"));
+
+
+
+// console.log(1 !== "0" || (true && "hey"));
+
+// bundan keyin   true qolganini hisoblamay ketvoradi dediz 
+
+// endi  siz 
+console.log(1 !== "0" || true && "hey");
+// bu bo'lganda  
+//  true || true bo'lib qoladi demoqchidiz
+// men true || hey bo'ladi deb o'ylagapman 
---
+++ task/table/scrript.js
@@ -0,0 +1,4 @@
+console.log(true == "1");
+console.log(true == "0");
+console.log(true == "a");
+console.log(true == "Salom");
